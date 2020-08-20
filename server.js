2829bb338d08875f714275cd5386ceb6fa39724c
Khalil <khalilfoulks@gmail.com>
2020-08-20 16:25:27 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+//Router import here
+// const AnimalsRouter = require('./animals.js/animalsRouter');
+
+//Global Middleware here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+res.send(`<h2> Node Testing Project is UP! </h2>`)
+
+})
+
+// server.use('/api/animals', AnimalsRouter);
+
+module.exports = server;
